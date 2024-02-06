f44f452236f2624939b06fad1d8b6c7ce15ecb11
lystosas <lystosas@gmail.com>
2024-02-06 17:19:02 -0500

    se cambia la version a la 20 y agregamos la dependencia MapStruct

--- src/main/java/com/co/glamping/model/Cliente.java
+++ src/main/java/com/co/glamping/model/Cliente.java
@@ -60,5 +60,4 @@ public class Cliente {
     @Column(name = "correo_electronico")
     private String correoElectronico;
 
-
 }
---
+++ src/main/java/com/co/glamping/service/IClienteService.java
@@ -0,0 +1,5 @@
+package com.co.glamping.service;
+
+public class IClienteService {
+
+}
